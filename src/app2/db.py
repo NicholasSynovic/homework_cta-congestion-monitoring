6bf5fa84f48bcabae9a9757f857769c4866ec700
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-10-23 15:32:27 -0500

    Add code to format line information data into a smaller format

---
+++ src/app2/db.py
@@ -0,0 +1,50 @@
+from typing import Any, List
+from urllib.parse import quote_plus
+
+from pymongo.collection import Collection
+from pymongo.database import Database
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+
+class DB:
+    def __init__(self, username: str, password: str, clusterURI: str) -> None:
+        self.database: Database | None = None
+        self.collection: Collection | None = None
+
+        self.username: str = quote_plus(string=username)
+        self.password: str = quote_plus(string=password)
+        self.uri: str = (
+            f"mongodb+srv://{self.username}:{self.password}@{clusterURI}"  # noqa: E501
+        )
+
+        self.client: MongoClient = MongoClient(
+            host=self.uri,
+            server_api=ServerApi("1"),
+        )
+
+    def ping(self) -> bool:
+        try:
+            self.client.admin.command(command="ping")
+        except Exception:
+            return False
+        else:
+            return True
+
+    def close(self) -> None:
+        self.client.close()
+
+    def getDatabase(self, databaseName: str) -> None:
+        self.database = self.client.get_database(name=databaseName)
+
+    def getCollection(
+        self,
+        collectionName: str,
+    ) -> None:
+        self.collection = self.database.get_collection(name=collectionName)
+
+    def writeDocumentsToCollection(
+        self,
+        documents: List[dict[str, Any]],
+    ) -> None:
+        self.collection.insert_many(documents=documents)
--- src/app2/main.py
+++ src/app2/main.py
@@ -2,8 +2,66 @@ from json import load
 from pathlib import Path
 
 import click
+from pandas import DataFrame, Series
 
+from src.app2.db import DB
 from src.app2.schema import Schema
+
+
+def formatData(json: dict) -> DataFrame:
+    df: DataFrame = DataFrame(data=json)
+    df.drop(
+        columns=[
+            ":@computed_region_awaf_s7ux",
+            ":@computed_region_6mkv_f3dw",
+            ":@computed_region_vrxf_vc4k",
+            ":@computed_region_bdys_3d7i",
+            ":@computed_region_43wa_7qmu",
+            "location",
+        ],
+        inplace=True,
+    )
+
+    df["line"] = None
+
+    row: Series
+    idx: int
+    for idx, row in df.iterrows():
+        if row["red"]:
+            df.loc[idx, "line"] = "red"
+        if row["blue"]:
+            df.loc[idx, "line"] = "blue"
+        if row["g"]:
+            df.loc[idx, "line"] = "green"
+        if row["brn"]:
+            df.loc[idx, "line"] = "brown"
+        if row["p"]:
+            df.loc[idx, "line"] = "purple"
+        if row["pexp"]:
+            df.loc[idx, "line"] = "purple-express"
+        if row["y"]:
+            df.loc[idx, "line"] = "yellow"
+        if row["pnk"]:
+            df.loc[idx, "line"] = "pink"
+        if row["o"]:
+            df.loc[idx, "line"] = "orange"
+
+    df.drop(
+        columns=[
+            "red",
+            "blue",
+            "g",
+            "brn",
+            "p",
+            "pexp",
+            "y",
+            "pnk",
+            "o",
+        ],
+        inplace=True,
+    )
+
+    return df
 
 
 @click.command()
@@ -35,13 +93,51 @@ from src.app2.schema import Schema
         path_type=Path,
     ),
 )
-def main(inputFile: Path, schemaFile: Path) -> None:
+@click.option(
+    "-u",
+    "--username",
+    "username",
+    required=True,
+    help="MongoDB Atlas username",
+    type=str,
+)
+@click.option(
+    "-p",
+    "--password",
+    "password",
+    required=True,
+    help="MongoDB Atlas password",
+    type=str,
+)
+@click.option(
+    "-c",
+    "--cluster-uri",
+    "clusterURI",
+    required=True,
+    help="MongoDB Atlas clusterURI",
+    type=str,
+)
+def main(
+    inputFile: Path,
+    schemaFile: Path,
+    username: str,
+    password: str,
+    clusterURI: str,
+) -> None:
     data: dict = load(fp=open(file=inputFile))
 
     schema: Schema = Schema(schemaPath=schemaFile)
 
     if schema.compare(data=data) is False:
         exit(1)
+
+    df: DataFrame = formatData(json=data)
+    print(df)
+
+    db: DB = DB(username=username, password=password, clusterURI=clusterURI)
+
+    if db.ping() is False:
+        exit(2)
 
 
 if __name__ == "__main__":
