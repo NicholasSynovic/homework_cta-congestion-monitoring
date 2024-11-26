0a0255ca573a045379c43d8f06c47b6416d68c58
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-11-26 16:50:31 -0600

    Add director

--- ingest/cta/directors/alert.py
+++ ingest/cta/directors/alert.py
@@ -33,3 +33,28 @@ class AlertAPIDirector:
         resp: requests.Response = requests.get(url=url, timeout=timeout)
 
         return resp
+
+    def getDetailedAlerts(
+        self,
+        timeout: int = 60,
+        activeonly: bool = None,
+        accessibility: bool = None,
+        planned: bool = None,
+        bystartdate: int = None,
+        recentdays: int = None,
+        routeid: typing.Optional[typing.List[str]] = None,
+        stationid: typing.Optional[typing.List[int]] = None,
+    ) -> str:
+        url: str = self.builder.buildDetailedAlertsAPIURL(
+            activeonly=activeonly,
+            accessibility=accessibility,
+            planned=planned,
+            bystartdate=bystartdate,
+            recentdays=recentdays,
+            routeid=routeid,
+            stationid=stationid,
+        )
+
+        resp: requests.Response = requests.get(url=url, timeout=timeout)
+
+        return resp
