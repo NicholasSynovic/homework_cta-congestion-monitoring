87720ebb513216c3a649bf3fdcf1dcf49e6f0274
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-11-26 21:01:46 -0600

    Get both rail and station alerts

--- ingest/ingest_CTAAlerts.py
+++ ingest/ingest_CTAAlerts.py
@@ -54,7 +54,7 @@ def main(outputFP: Path, logDirectory: Path) -> None:
     logging.info("Sending get query to Route Status API")
 
     try:
-        df: DataFrame = api.route_status(type=["station"])
+        df: DataFrame = api.route_status(type=["rail", "station"])
     except ValueError as ve:
         logging.error("Status code not 200")
         logging.exception(msg=ve)
