5adf1f90c838bb4884c860b5b02f40210fc2289a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-11-26 20:46:46 -0600

    Add logging

--- ingest/getCTAAlerts.py
+++ ingest/getCTAAlerts.py
@@ -1,4 +1,6 @@
+import logging
 from pathlib import Path
+from time import time
 
 import click
 from cta.alert import AlertAPI
@@ -20,11 +22,45 @@ from pandas import DataFrame
         path_type=Path,
     ),
 )
-def main(outputFP: Path) -> None:
-    api: AlertAPI = AlertAPI()
-    df: DataFrame = api.route_status(type=["station"])
+@click.option(
+    "-l",
+    "--logs",
+    "logDirectory",
+    required=True,
+    help="Directory to store logs",
+    type=click.Path(
+        exists=True,
+        dir_okay=True,
+        writable=True,
+        resolve_path=True,
+        path_type=Path,
+    ),
+)
+def main(outputFP: Path, logDirectory: Path) -> None:
+    logFP: Path = Path(logDirectory, f"cta-alerts.{time()}.log")
 
-    df.to_json(path_or_buf=outputFP, indent=4, orient="records")
+    logging.basicConfig(
+        filename=logFP,
+        filemode="w",
+        format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
+        datefmt="%H:%M:%S",
+        level=logging.DEBUG,
+    )
+
+    logging.info(msg="Started application")
+
+    api: AlertAPI = AlertAPI()
+
+    logging.info("Sending get query to Route Status API")
+
+    try:
+        df: DataFrame = api.route_status(type=["station"])
+    except ValueError as ve:
+        logging.error("Status code not 200")
+        logging.exception(msg=ve)
+    else:
+        df.to_json(path_or_buf=outputFP, indent=4, orient="records")
+        logging.info(msg=f"Wrote JSON to {outputFP}")
 
 
 if __name__ == "__main__":
