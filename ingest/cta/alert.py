69678ff37f7d32093394e7e6703d98dbfe1ab4a9
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-11-26 19:49:45 -0600

    Implement alert API interface

---
+++ ingest/cta/alert.py
@@ -0,0 +1,39 @@
+import cta
+import cta.directors
+import cta.directors.alert
+import pandas
+import requests
+
+
+class AlertAPI:
+    def __init__(self) -> None:
+        self.director: cta.directors.alert.AlertAPIDirector = (
+            cta.directors.alert.AlertAPIDirector()
+        )
+
+    def route_status(self, **kwargs) -> pandas.DataFrame:
+        resp: requests.Response = self.director.getRouteStatus(**kwargs)
+
+        if resp.status_code != 200:
+            raise ValueError("Route Status API endpoint status code != 200")
+
+        ctaRoutes: dict = resp.json()["CTARoutes"]["RouteInfo"]
+
+        df: pandas.DataFrame = pandas.DataFrame(data=ctaRoutes)
+        df["RouteURL"] = df["RouteURL"].str.get("#cdata-section")
+        df.reset_index(drop=True, inplace=True)
+
+        return df
+
+    def detailed_status(self, **kwargs) -> pandas.DataFrame:
+        resp: requests.Response = self.director.getDetailedAlerts(**kwargs)
+
+        if resp.status_code != 200:
+            raise ValueError("Detailed Status API endpoint status code != 200")
+
+        ctaRoutes: dict = resp.json()["CTARoutes"]["RouteInfo"]
+
+        df: pandas.DataFrame = pandas.DataFrame(data=ctaRoutes)
+        df.reset_index(drop=True, inplace=True)
+
+        return df
