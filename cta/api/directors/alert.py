2f503fd36e865cc6950ae69db8b17eccb72499c4
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-12-05 08:33:14 -0600

    Reduce the number of parameter inputs and set up train director

--- cta/api/directors/alert.py
+++ cta/api/directors/alert.py
@@ -1,6 +1,4 @@
-import typing
-
-import requests
+from requests import Response, get
 
 from cta.api.builders.alert import AlertAPIBuilder
 
@@ -16,22 +14,7 @@ class AlertAPIDirector:
         """
         self.builder: AlertAPIBuilder = AlertAPIBuilder()
 
-    def getRouteStatus(
-        self,
-        timeout: int = 60,
-        type: typing.Optional[
-            typing.List[
-                typing.Literal[
-                    "bus",
-                    "rail",
-                    "station",
-                    "systemwide",
-                ]
-            ]
-        ] = None,
-        routeid: typing.Optional[typing.List[str]] = None,
-        stationid: typing.Optional[typing.List[int]] = None,
-    ) -> requests.Response:
+    def getRouteStatus(self, timeout: int = 60, **kwargs) -> Response:
         """
         Construct and get the response from the Route Status API.
 
@@ -40,29 +23,15 @@ class AlertAPIDirector:
         :param timeout: Timeout in seconds for when to close the HTTP connection, defaults to 60
         :type timeout: int, optional
         :return: The HTTP GET response
-        :rtype: requests.Response
+        :rtype: Response
         """  # noqa: E501
-        url: str = self.builder.buildRouteStatusAPIURL(
-            type=type,
-            routeid=routeid,
-            stationid=stationid,
-        )
+        url: str = self.builder.buildRouteStatusAPIURL(**kwargs)
 
-        resp: requests.Response = requests.get(url=url, timeout=timeout)
+        resp: Response = get(url=url, timeout=timeout)
 
         return resp
 
-    def getDetailedAlerts(
-        self,
-        timeout: int = 60,
-        activeonly: bool = None,
-        accessibility: bool = None,
-        planned: bool = None,
-        bystartdate: int = None,
-        recentdays: int = None,
-        routeid: typing.Optional[typing.List[str]] = None,
-        stationid: typing.Optional[typing.List[int]] = None,
-    ) -> str:
+    def getDetailedAlerts(self, timeout: int = 60, **kwargs) -> Response:
         """
         Construct and get the response from the Detailed Alert Status API.
 
@@ -71,18 +40,10 @@ class AlertAPIDirector:
         :param timeout: Timeout in seconds for when to close the HTTP connection, defaults to 60
         :type timeout: int, optional
         :return: The HTTP GET response
-        :rtype: requests.Response
+        :rtype: Response
         """  # noqa: E501
-        url: str = self.builder.buildDetailedAlertsAPIURL(
-            activeonly=activeonly,
-            accessibility=accessibility,
-            planned=planned,
-            bystartdate=bystartdate,
-            recentdays=recentdays,
-            routeid=routeid,
-            stationid=stationid,
-        )
+        url: str = self.builder.buildDetailedAlertsAPIURL(**kwargs)
 
-        resp: requests.Response = requests.get(url=url, timeout=timeout)
+        resp: Response = get(url=url, timeout=timeout)
 
         return resp
