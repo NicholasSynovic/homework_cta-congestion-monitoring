898271a6260a1141fe47994d1d77faa0a68d2c2a
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2024-12-05 08:43:32 -0600

    Simplify import structure

--- cta/api/system.py
+++ cta/api/system.py
@@ -1,5 +1,5 @@
-import pandas
-import requests
+from pandas import DataFrame
+from requests import Response, get
 
 
 class SystemAPI:
@@ -10,15 +10,15 @@ class SystemAPI:
     def __init__(self) -> None:
         pass
 
-    def getLStopList(self) -> pandas.DataFrame:
+    def getLStopList(self) -> DataFrame:
         """
         Get data from the L Stop List API endpoint.
 
         :raises ValueError: Raised if the response status code != 200
-        :return: A pandas.DataFrame of the response
-        :rtype: pandas.DataFrame
+        :return: A DataFrame of the response
+        :rtype: DataFrame
         """
-        resp: requests.Response = requests.get(
+        resp: Response = get(
             url="https://data.cityofchicago.org/resource/8pix-ypme.json",
             timeout=60,
         )
@@ -26,7 +26,7 @@ class SystemAPI:
         if resp.status_code != 200:
             raise ValueError("System API endpoint status code != 200")
 
-        df: pandas.DataFrame = pandas.DataFrame(data=resp.json())
+        df: DataFrame = DataFrame(data=resp.json())
         df["latitude"] = df["location"].str.get("latitude")
         df["longitude"] = df["location"].str.get("longitude")
         df.drop(columns=["location"], inplace=True)
